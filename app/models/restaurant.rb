8e6b6c87dc014f12b861fcc09e23c66dc800cdb7
Melissa Augier <augiermelissa@gmail.com>
2020-02-12 15:37:55 +0100

    yelp done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, :dependent => :destroy
+  CATEGORIES = %w(chinese italian japanese french belgian)
+  has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Creating restaurants...'
+tour_d_argent = Restaurant.new(
+  name: "La Tour d'Argent",
+  address: '282 Kevin Brook',
+  phone_number: '06 24 84 85 67',
+  category: 'belgian')
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(
+  name: "Chez Gladines",
+  address: '83 Gladines Brook',
+  phone_number: '+33 24 44 85 67',
+  category: 'french')
+chez_gladines.save!
+
+masaniello = Restaurant.new(
+  name: "Masaniello",
+  address: '83 rue Gambetta',
+  phone_number: '05 56 44 85 67',
+  category: 'italian')
+masaniello.save!
+
+tap_tim = Restaurant.new(
+  name: "Tap Tim",
+  address: '6 rue de la grosse cloche',
+  phone_number: '05 57 00 85 67',
+  category: 'chinese')
+tap_tim.save!
+
+saveur_japanese = Restaurant.new(
+  name: "Saveur Japanese",
+  address: '10 rue de la grosse cloche',
+  phone_number: '05 57 00 85 67',
+  category: 'japanese')
+saveur_japanese.save!
+
+puts 'Finished!'
